65f20b561766ce2490583cecb7714c1f86338cf3
Priyanshu Gupta <142195022+Priyanshugupta20@users.noreply.github.com>
2025-08-21 03:08:39 +0530

    Update feature_scaling.py

--- backend/src/feature_scaling.py
+++ backend/src/feature_scaling.py
@@ -1,8 +1,9 @@
-from sklearn.preprocessing import MinMaxScaler
+# from sklearn.preprocessing import MinMaxScaler
 
-def scale_numerical_columns(df):
-    """Scale all numeric columns between 0 and 1."""
-    scaler = MinMaxScaler()
-    num_cols = df.select_dtypes(include=['float64', 'int64']).columns
-    df[num_cols] = scaler.fit_transform(df[num_cols])
-    return df
+# def scale_numerical_columns(df):
+#     """Scale all numeric columns between 0 and 1."""
+#     scaler = MinMaxScaler()
+#     num_cols = df.select_dtypes(include=['float64', 'int64']).columns
+#     df[num_cols] = scaler.fit_transform(df[num_cols])
+
+#     return df
